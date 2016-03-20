04f9a279a740902231e2e63689d493573d0b451d
Michael Symmes <msymmes@gmail.com>
2016-03-19 20:34:20 -0400

    Implemented module pattern.

--- hotkeyConfig.js
+++ hotkeyConfig.js
@@ -1,21 +1,37 @@
-document.addEventListener('DOMContentLoaded', function(evt) {
-  var templates = document.getElementById('hotkeyConfigTemplate').import;
-  var template = templates.getElementById('hotkey-setting');
+var hotkeyCommander = (function() {
 
-  // hotkey settings will need to be stored
-  // most likely in local storage
-  // but we need to make concession for other places
-  var hotkeys = {
-    BUY: 's',
-    SELL: 'l'
+  var template;
+
+  var public_api = {
+    init: init,
+    loadKeys: loadKeys
   };
 
+  return public_api;
 
-  for (var i in hotkeys) {
-    var clone = document.importNode(template.content, true);
-    clone.querySelector('.functionLabel').innerText = i;
-    clone.querySelector('.settingLabel').innerText = hotkeys[i];
-    document.getElementById('hotkeys').appendChild(clone);
+  // load template
+  function init() {
+    var templates = document.getElementById('hotkeyConfigTemplate').import;
+    template = templates.getElementById('hotkey-setting');
   }
 
-});
+  function loadKeys(hotkeys, el) {
+    // load hotkeys into the template
+    // and write it to the page.
+    for (var i in hotkeys) {
+      var clone = document.importNode(template.content, true);
+
+      clone.querySelector('.settingButton')
+        .addEventListener("click", function(evt) {
+          console.log(this);
+        });
+
+      clone.querySelector('.functionLabel').innerText = i;
+      clone.querySelector('.settingLabel').innerText = hotkeys[i];
+      el.appendChild(clone);
+    }
+  }
+
+
+
+})();
