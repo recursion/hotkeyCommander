888e4e756f0856eabe8db765e1a5cfa0cf78cf45
Michael Symmes <msymmes@gmail.com>
2016-03-21 19:02:39 -0400

    Updated with a proper hotkey object model.

--- hotkeyConfig.js
+++ hotkeyConfig.js
@@ -4,7 +4,8 @@ var hotkeyCommander = (function() {
 
   var public_api = {
     init: init,
-    loadKeys: loadKeys
+    loadKeys: loadKeys,
+    buildKeyMap: buildKeyMap
   };
 
   return public_api;
@@ -20,18 +21,49 @@ var hotkeyCommander = (function() {
     // and write it to the page.
     for (var i in hotkeys) {
       var clone = document.importNode(template.content, true);
+      console.log(i, ' ',  hotkeys[i]);
 
       clone.querySelector('.settingButton')
         .addEventListener("click", function(evt) {
           console.log(this);
         });
 
-      clone.querySelector('.functionLabel').innerText = i;
-      clone.querySelector('.settingLabel').innerText = hotkeys[i];
+      clone.querySelector('.functionLabel').innerText = stripUnderscores(hotkeys[i]);
+      clone.querySelector('.settingLabel').innerText = String.fromCharCode(i);
       el.appendChild(clone);
     }
   }
 
+  // takes in an object of mapped hotkeys
+  // and builds an object with keyCode keys
+  // so that the hotkeys can be accessed by their keycode
+  // instead of by their name
+  // this is what would be used by the hotkey engine
+  function buildKeyMap(hotkeys) {
+    var result = {};
 
+    for(var hotkey in hotkeys){
+      if(hotkeys.hasOwnProperty(hotkey)){
+        // build new hotkey dictionary here
+        var thisKey = hotkeys[hotkey];
+        result[thisKey.keyCode] = {
+          name: hotkey,
+          ctrlKey: hotkeys[hotkey].ctrlKey,
+          altKey: hotkeys[hotkey].altKey,
+          shiftKey: hotkeys[hotkey].shiftKey,
+        };
+      }
+    }
+    return result;
+  }
+
+
+  //       HELPERS
+
+  // use the hotkey data to construct a descriptor
+  function stripUnderscores(hotkeyObject){
+    var name = hotkeyObject.name.replace('_', ' ');
+    return name;
+  }
 
 })();
