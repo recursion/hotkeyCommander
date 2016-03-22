1aac631cc83e58d5b2851fca74384fe3427ebcd0
Michael Symmes <msymmes@gmail.com>
2016-03-21 21:08:38 -0400

    Added hotkey recording.

--- hotkeyCommander.js
+++ hotkeyCommander.js
@@ -3,21 +3,33 @@ var hotkeyCommander = (function() {
   var template;
 
   var public_api = {
-    init: init,
-    loadKeys: loadKeys,
-    buildKeyMap: buildKeyMap
+    init: init
   };
 
   return public_api;
 
+  // the original keys (before mapping);
+  var keyDictionary = {};
+
+  // an object with keycodes for keys
+  // this is what the hotkey engine will call when keys are pressed
   var keymap = {};
+  var recording = false;
+  var rootElement;
 
   // load template
   function init(hotkeys, el) {
     var templates = document.getElementById('hotkeyTemplate').import;
     template = templates.getElementById('hotkey-setting');
-    var keymap = buildKeyMap(hotkeys);
-    loadKeys(keymap, el);
+
+    // store the original hotkeys
+    // TODO: move these into local storage after initial load
+    keyDictionary = hotkeys;
+    rootElement = el;
+
+    reload(keyDictionary, rootElement);
+
+    window.addEventListener('keydown', onkeydown);
   }
 
   function loadKeys(hotkeys, el) {
@@ -29,7 +41,15 @@ var hotkeyCommander = (function() {
 
       clone.querySelector('.settingButton')
         .addEventListener("click", function(evt) {
-          console.log('Attempting to change key: ', i);
+          if (!recording) {
+            // add highlighting to bg for this element
+            this.parentElement.style.background = 'red';
+            console.log('Attempting to change key: ', i);
+            console.log(keymap[i]);
+            recording = i;
+            // popup a modal that lets the user know whats up
+          }
+
         });
 
       clone.querySelector('.functionLabel').innerText = stripUnderscores(hotkeys[i]);
@@ -61,13 +81,89 @@ var hotkeyCommander = (function() {
     return result;
   }
 
+  // rebuild the keymap
+  // render the hotkeys element
+  function reload(hotkeys, el) {
+    keymap = buildKeyMap(hotkeys);
+
+    el.innerHTML = '';
+    //load the hotkeys into the DOM
+    loadKeys(keymap, el);
+  }
+
+  function onkeydown(evt) {
+    // already recording - this is the new key
+    if (recording) {
+      console.log('You are trying to change ', recording, ' to : ', evt.keyCode);
+
+      //TODO: validate that the key is not already in use.
+
+      var keyToSet = findKeyByCode(recording);
+      // set the key in the hotkey dictionary
+      if (keyToSet) {
+        keyToSet.keyCode = evt.keyCode;
+        reload(keyDictionary, rootElement);
+      } else {
+        console.error('Could not find key: ', recording);
+        console.log('Proper keys are: ', keymap);
+      }
+
+      // remove bg highlighting
+      var keys = document.getElementsByClassName('hotkeyConfig');
+      for (var key in keys) {
+        if (keys.hasOwnProperty(key)){
+          keys[key].style.background = '';
+        }
+      }
+      recording = false;
+
+    } else {
+      if (keymap[evt.keyCode]){
+        console.log('Keymap exists');
+        console.log(keymap[evt.keyCode]);
+      } else {
+        console.log('No key mapped to ', evt.keyCode);
+      }
+    }
+  }
 
   //       HELPERS
 
-  // use the hotkey data to construct a descriptor
+  /**
+   * searches through the keyDictionary and
+   * attempts to find the key containing
+   * the passed in keycode
+   * @returns {Key Object} or null
+  */
+  function findKeyByCode(keycode) {
+    for (var key in keyDictionary){
+      console.log(key, ' ', keyDictionary[key]);
+      if (+keyDictionary[key].keyCode === +keycode) {
+        return keyDictionary[key];
+      }
+    }
+    return null
+  }
+
   function stripUnderscores(hotkeyObject){
     var name = hotkeyObject.name.replace('_', ' ');
     return name;
+  }
+
+  // turn a snake case string into a camelCase string
+  function snakeCaseToCamelCase(string) {
+    string = string.replace('_', ' ');
+    string = string.split(' ');
+    var result = string.map(function(word, index){
+      // uppercase the first character of each word
+      // except the first word
+      if(index !== 0) {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+      } else {
+        return word.toLowerCase();
+      }
+    });
+    return result.join('');
   }
 
 })();
