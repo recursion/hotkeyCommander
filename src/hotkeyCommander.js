5375e8c4a0b95f3231aac3ec7bdbe1ab158d8626
Michael Symmes <msymmes@gmail.com>
2016-04-01 03:10:34 -0400

    linter cleanup

--- src/hotkeyCommander.js
+++ src/hotkeyCommander.js
@@ -1,27 +1,28 @@
 const configurator = require('./configurator')
 const engine = require('./engine')
 
-let engineSet = false
-let configuratorSet = false
-
 // two ways to start the module:
 //   1) call init with the proper parameters (this assumes 1 context)
 //   2) initialized engine and configurator seperately
 
+module.exports = {
+  init: init,
+  startEngine: startEngine,
+  startConfigurator: startConfigurator
+}
+
 // if only 1 element is passed in,
 // we can assume that its ok to
 // do everything in one context?
-exports.init = (configContainerEl, configListener, engineListener) => {
+function init (configContainerEl, configListener, engineListener) {
   configurator.init(configContainerEl, configListener || window)
   engine.init(engineListener || window)
 }
 
-exports.startEngine = (listenerEl) => {
+function startEngine (listenerEl) {
   engine.init(listenerEl)
-  engineSet = true
 }
 
-exports.startConfigurator = (containerEl, listenerEl) => {
+function startConfigurator (containerEl, listenerEl) {
   configurator.init(containerEl, listenerEl)
-  configuratorSet = true
 }
--- src/utils.js
+++ src/utils.js
@@ -1,44 +1,40 @@
 // Common modules
 
-const utils = exports;
+const utils = exports
 
 utils.addListener = (listenerEl, listenerType, cb) => {
-  listenerEl = listenerEl || window;
-  listenerEl.addEventListener(listenerType, cb);
-};
-
-
+  listenerEl = listenerEl || window
+  listenerEl.addEventListener(listenerType, cb)
+}
 
 utils.stripUnderscores = (string) => {
   if (!string || typeof string !== 'string') {
     console.error('stripUnderscores requires a string')
-    debugger
     return ''
   }
-  return string.replace('_', ' ');
+  return string.replace('_', ' ')
 }
 
 // turn a snake case string into a camelCase string
 utils.snakeCaseToCamelCase = (string) => {
-  string = stripUnderscores(string);
-  string = string.split(' ');
-  const result = string.map(function(word, index){
+  string = utils.stripUnderscores(string)
+  string = string.split(' ')
+  const result = string.map(function (word, index) {
     // uppercase the first character of each word
     // except the first word
-    if(index !== 0) {
-      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    if (index !== 0) {
+      return word[0].toUpperCase() + word.slice(1).toLowerCase()
     } else {
-      return word.toLowerCase();
+      return word.toLowerCase()
     }
-  });
-  return result.join('');
+  })
+  return result.join('')
 }
 
-
-
+/* globals Window Element */
 // validate that a object is an Element or Window
 utils.validateEl = (el) => {
-  if (!el instanceof Window || !el instanceof Element){
+  if (!el instanceof Window || !el instanceof Element) {
     return false
   } else {
     return true
